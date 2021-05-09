036a4739656f8104094b78903ac3f971dd9d8350
NR4l3rt0 <alberto.ruizm@outlook.com>
2021-05-09 06:53:00 +0200

    vuelve a funcionar la app aparentemente

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import BarraNavegacion from './componentes/BarraNavegacion';
+import Producto from './componentes/Producto';
+import Home from './componentes/Home';
+import ClienteCRM from './componentes/ClienteCRM';
+import EmpleadoRRHH from './componentes/EmpleadoRRHH';
+import Analytics from './componentes/Analytics';
+import Footer from './componentes/Footer';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+  /* Y dentro de cada uno otra vez enroutar 
+
+  */ 
+    <Router>
+      <BarraNavegacion sticky="top"/> 
+        <Switch>
+        <Route path="/" exact component={Home}/> 
+          <Route path="/area-cliente" exact component={ClienteCRM}/>
+          <Route path="/productos" exact component={Producto}/>
+          <Route path="/empleados" exact component={EmpleadoRRHH}/>
+          <Route path="/analytics" exact component={Analytics}/>
+        </Switch>
+      <Footer />
+    
+    </Router>
   );
 }
+
+
+
+
 
 export default App;
